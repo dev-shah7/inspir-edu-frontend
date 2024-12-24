74c90d1ba2f88a608d7a469755c324c9ba711090
dev-shah7 <s.tech4747@gmail.com>
2024-12-25 03:32:32 +0500

    check deployment

--- src/routes/AdminRoutes.jsx
+++ src/routes/AdminRoutes.jsx
@@ -5,8 +5,9 @@ import Layout from "../components/Layout/Layout";
 import Dashboard from "../admin/Dashboard";
 import Courses from "../admin/courses/Courses";
 import Modules from "../admin/modules/Modules";
-import Questions from "../admin/questions/Questions";
+
 import MainContent from "../admin/MainContent";
+import Questions from "../admin/questions/Questions";
 
 const AdminRoutes = () => {
   return (

db8f2cae3cab0a77458178724be4a4899307864b
dev-shah7 <s.tech4747@gmail.com>
2025-06-02 05:00:06 +0500

    check for media type

--- src/student/modules/CurrentModuleMedia.jsx
+++ src/student/modules/CurrentModuleMedia.jsx
@@ -1,32 +1,47 @@
-import React from 'react';
-import VideoPlayer from '../../components/common/VideoPlayer/VideoPlayer';
-import PdfViewer from '../../components/common/PDFViewer/PDFViewer';
-import ImageViewer from '../../components/common/ImageViewer/ImageViewer';
-import useModuleStore from '../../admin/store/useModuleStore';
+import React from "react";
+import VideoPlayer from "../../components/common/VideoPlayer/VideoPlayer";
+import PdfViewer from "../../components/common/PDFViewer/PDFViewer";
+import ImageViewer from "../../components/common/ImageViewer/ImageViewer";
+import useModuleStore from "../../admin/store/useModuleStore";
 import playVideoPoster from "../../assets/play-thumbnail.jpg";
 
-const CurrentModuleMedia = ({ isWatched, setIsWatched, onDuration, onVideoEnd }) => {
-  const { currentModule, lastPlayPosition, userModuleId, isFullVideoWatched } = useModuleStore();
+const CurrentModuleMedia = ({
+  isWatched,
+  setIsWatched,
+  onDuration,
+  onVideoEnd,
+}) => {
+  const { currentModule, lastPlayPosition, userModuleId, isFullVideoWatched } =
+    useModuleStore();
   const moduleType = currentModule?.data?.type || 1;
 
-  return <div className='mx-5'>
-    {moduleType === 1 && (
-      <VideoPlayer 
-        videoUrl={currentModule?.data?.url} 
-        lastPlayPosition={Number(lastPlayPosition) || 0}
-        posterUrl={playVideoPoster} 
-        isWatched={isWatched} 
-        setIsWatched={setIsWatched}
-        onDuration={onDuration}
-        moduleId={userModuleId}
-        onVideoEnd={onVideoEnd}
-        isFullVideoWatched={isFullVideoWatched}
-      />
-    )}
-    {moduleType === 2 && <PdfViewer fileUrl={currentModule?.data?.url} />}
-    {moduleType === 3 && <ImageViewer imageUrl={currentModule.data.url}
-      altText={currentModule.data.name || 'Course image'} />}
-  </div>;
+  return (
+    <div className="mx-5">
+      {moduleType === 1 && (
+        <VideoPlayer
+          videoUrl={currentModule?.data?.url}
+          lastPlayPosition={Number(lastPlayPosition) || 0}
+          posterUrl={playVideoPoster}
+          isWatched={isWatched}
+          setIsWatched={setIsWatched}
+          onDuration={onDuration}
+          moduleId={userModuleId}
+          onVideoEnd={onVideoEnd}
+          isFullVideoWatched={isFullVideoWatched}
+        />
+      )}
+      {moduleType === 2 ||
+        (moduleType === null && (
+          <PdfViewer fileUrl={currentModule?.data?.url} />
+        ))}
+      {moduleType === 3 && (
+        <ImageViewer
+          imageUrl={currentModule.data.url}
+          altText={currentModule.data.name || "Course image"}
+        />
+      )}
+    </div>
+  );
 };
 
 export default CurrentModuleMedia;
